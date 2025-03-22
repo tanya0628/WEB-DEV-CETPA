93665ef2d24fe61708ec0e8d118cca63f00d76a2
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-22 15:39:46 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day2/main.js
@@ -0,0 +1,57 @@
+// Hoisting
+
+//case of giving outout
+// num = 40
+// console.log(num)
+
+
+//case of undefined
+// var num;
+// console.log(num)
+
+
+//case of not defined error
+// console.log(num)
+
+
+//Error
+
+// num = 40;
+// var num;
+// console.log(num)
+
+
+//not error
+// num = 40
+// console.log(num)
+// var num
+
+
+// Operators
+//Arithmatic Operators
+
+let num1 = 24
+let num2 = 12
+
+console.log(num1 + num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1 % num2)
+
+//Comparison Operators
+let num3 = '24'
+
+console.log(num1 == num2)
+console.log(num1 === num2)
+console.log(num1 === num3)
+console.log(num1 == num3)
+
+console.log(num1 != num2)
+console.log(num1 !== num3)
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num2 < num3)
+
+//  <= , >= are also comparison operators
