1cae89593509822f988e7dcc35af8ab976e973dc
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-22 15:38:37 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day1/main.js
@@ -0,0 +1,22 @@
+// console.log("hello india");
+// var a = 50;
+// var a = 56;
+// console.log(a);
+
+
+// let b = 55;
+//   let o = 58;
+// console.log(b);
+
+// const c = 50;
+// //  const c = 89;
+// console.log(c);  // hello//
+
+//  let num = "40";
+//  let num2 = "40";
+// console.log(num !== num2);
+
+
+let a = 80;
+console.log( a++,++a, a , a, a, a++, a++, a++, a , ++a , a ,++a, ++a,++a, a++, a++)
+// 80, 82 , 82, 82,82, 
