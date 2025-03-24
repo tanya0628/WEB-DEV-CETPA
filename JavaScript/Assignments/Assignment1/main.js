af93516e5fce31cfde40ef47cef3ec78df984de4
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-24 13:43:30 +0530

    Update main.js

--- JavaScript/Assignments/Assignment1/main.js
+++ JavaScript/Assignments/Assignment1/main.js
@@ -4,84 +4,84 @@ var b = 44;
 var sum = a + b;
 console.log("the sum of the numbers is " + sum);
 
-// var a = parseInt(prompt("enter no. "));
-// var b = parseInt(prompt("enter no. ")) ;
-// var sum = a + b;
-// console.log("the sum of the numbers is " +  sum );
+var a = parseInt(prompt("enter no. "));
+var b = parseInt(prompt("enter no. ")) ;
+var sum = a + b;
+console.log("the sum of the numbers is " +  sum );
 
 
 
-//question 2 Find if the conditions are obeyed or not?
-// var a = 6;
-// var b = 5;
+question 2 Find if the conditions are obeyed or not?
+var a = 6;
+var b = 5;
 
-// if(a<10 && a>b){
-//     console.log("the condition is true");
+if(a<10 && a>b){
+    console.log("the condition is true");
 
-// }
-// else{
-//     console.log("false");
+}
+else{
+    console.log("false");
 
-// }
+}
 
-//question 3 Check the conditions
+question 3 Check the conditions
 
-// var a = 10;
-// var b = 50;
-// if (a % 10 == 0 && b % 10 == 0) {
-//     console.log("true");
-
-
-// }
-// else if (a % 10 != 0 && b % 10 != 0) {
-//     console.log("false");
-
-// }
-// else if (a % 10 == 0 || b % 10 == 0) {
-//     console.log("true");
-
-// }
+var a = 10;
+var b = 50;
+if (a % 10 == 0 && b % 10 == 0) {
+    console.log("true");
 
 
-// question 4 Find the first digit of a 4 digit number 
-// let number = 4567;
-// let firstDigit = Math.floor(number / 1000);
-// console.log(firstDigit);  // Output: 4
+}
+else if (a % 10 != 0 && b % 10 != 0) {
+    console.log("false");
+
+}
+else if (a % 10 == 0 || b % 10 == 0) {
+    console.log("true");
+
+}
 
 
-
-
-
-//question 5  Find the last digit of a 4 digit number 
-// let number = 4567;
-// let firstDigit = Math.floor(number % 10);
-// console.log(firstDigit);  // Output: 7
+question 4 Find the first digit of a 4 digit number 
+let number = 4567;
+let firstDigit = Math.floor(number / 1000);
+console.log(firstDigit);  // Output: 4
 
 
 
 
-//question 6 Find the remainder
 
-//  let a = 9;
-//  let b = 2;
-//  result = a % b;
-
-//  console.log(result);
+question 5  Find the last digit of a 4 digit number 
+let number = 4567;
+let firstDigit = Math.floor(number % 10);
+console.log(firstDigit);  // Output: 7
 
 
-// question 7 Multiply two Numbers
-// let a = 2;
-//  let b = 5;
-//  result = a * b ;
-
-//  console.log(result);
 
 
-// question 8  Marks Calculator
-// let a = 50;
-// let b = 20;
-// let c = 100;
+question 6 Find the remainder
+
+ let a = 9;
+ let b = 2;
+ result = a % b;
+
+ console.log(result);
+
+
+question 7 Multiply two Numbers
+let a = 2;
+ let b = 5;
+ result = a * b ;
+
+ console.log(result);
+
+
+question 8  Marks Calculator
+let a = 50;
+let b = 20;
+let c = 100;
   
-// let result = a + b + c;
-// console.log(result);
+let result = a + b + c;
+console.log(result);
 
