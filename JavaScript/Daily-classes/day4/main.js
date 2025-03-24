d0c7b0c22d386110fa073b9310807ed7cb8c22c4
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-24 13:40:36 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day4/main.js
@@ -0,0 +1,97 @@
+// console.log("hello world");
+// for loop
+// let a = 5;
+// for (let i = 0; i < a; i++) {
+//     // console.log("i am a programmer");
+//     console.log(a);
+
+
+// }
+
+// let n = 0;
+// for (let i = 4; i > n; i--) {
+//     console.log("i am a programmer");
+// }
+
+
+
+// for in loop
+
+// let obj = {
+//         a: 67,
+//         b: 99,
+//         c: 64,
+//     }
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element);
+
+//     }
+// }
+
+// let obj = {
+//     name: "aman",
+//     age: 25,
+// }
+// for (const key in obj) {
+//     console.log(`${key} = ${obj[key]}`);
+
+// }
+
+// let student = {
+//     name: "monika",
+//     id: 5683,
+//     age: 18,
+// }
+
+// for (const key in student) {
+//     console.log(`${key} = ${student[key]}`);
+
+
+// }
+
+
+
+// table
+// let n = prompt("enter the number");
+
+// let res = 0
+// for (i = 1; i <= 10; i++) {
+//     res = n * i
+//     console.log(`${n} * ${i}  = ${res}`);
+
+// }
+// let n = prompt("enter the no.");
+// let res = 0;
+// let i = 1;
+// while (i <= 10) {
+//     res = n * i
+//     console.log(`${n} * ${i}  = ${res}`);
+//     i++;
+
+
+// }
+
+
+
+
+// while loop
+// let i = 1;
+// while (i < 10) {
+//     console.log("the number is " + i);
+//     i++
+// }
+
+
+// do while loop
+let i = 1;
+do {
+    console.log("hey...");
+    i++;
+
+} while (i < 10);
+
+
+
+
