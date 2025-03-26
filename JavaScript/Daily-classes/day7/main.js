b19be72b4ed44927fe44834f606df90dc43acf60
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-26 14:20:22 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day7/main.js
@@ -0,0 +1,43 @@
+let arr = ["hello", "everyone", "gud", "morning"];
+
+// arr.push("arrays" )// push method
+// let res = arr.pop()
+//  let res = arr.slice(0, 2); // slice method
+//  let res = arr.toString(); 
+// let res = arr.shift()
+// let res = arr.unshift("its summer");
+// console.log(res);
+
+//map method
+// let arr1 = [3, 5, 7, 9]
+// let res1 = arr1.map((x) => x * 3);
+// console.log(res1);
+
+
+// let arr2 = ["we" , "are",  "learning" ,  "java"]
+// let res = arr2.filter(word => word.length >= 3 )
+// console.log(res);
+
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let res = arr3.find(x => x == 2)
+// console.log(res);
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let res = arr3.findIndex(x => x == 2)
+// console.log(res);
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let res = arr3.includes(1)
+// console.log(res);
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let res = arr3.reverse()
+// console.log(res);
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let res = arr3.splice(0, 4, 20)
+// console.log(res);
+
+
+
