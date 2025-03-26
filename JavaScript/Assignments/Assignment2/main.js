3eaa2feddc5f3f3de37ba128eb3c0d84e45e271a
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-26 14:21:31 +0530

    Add files via upload

---
+++ JavaScript/Assignments/Assignment2/main.js
@@ -0,0 +1,95 @@
+// quesion 1
+// let marks = 35;
+// let grade;
+// switch (true) {
+//   case marks >= 41 && marks <= 50:
+//     grade = "A";
+
+//     break;
+
+//   case marks >= 31 && marks <= 40:
+//     grade = "B";
+
+//     break;
+
+//   case marks >= 21 && marks <= 30:
+//     grade = "C";
+
+//     break;
+
+//   case marks >= 11 && marks <= 20:
+//     grade = "D";
+
+//     break;
+
+//   case marks >= 0 && marks <= 10:
+//     grade = "E";
+
+//     break;
+
+//   default:
+//     grade = " invalid marks";
+//     break;
+// }
+
+// console.log(grade);
+
+// let inputChar = prompt("Enter a character: ");
+
+// if (inputChar === 'p') {
+//     console.log("PrepBytes");
+// } else if (inputChar === 'z') {
+//     console.log("Zenith");
+// } else if (inputChar === 'e') {
+//     console.log("Expert Coder");
+// } else if (inputChar === 'd') {
+//     console.log("Data Structure");
+// } else {
+//     console.log("Character not found in table");
+// }
+
+// let a =  prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number") ;
+//  if(a >= b && a >= c){
+//     console.log(a);
+
+//  }
+//  else if(b >= a && b >= c){
+//     console.log(b);
+
+//  }
+//  else{
+//     console.log(c);
+
+//  }
+
+// let a =  prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number") ;
+
+// if(a > b && a < c){
+//         console.log(a);
+
+//      }
+//      else if(b > a && b < c){
+//         console.log(b);
+
+//      }
+//      else{
+//         console.log(c);
+
+//      }
+
+// var a = 20;
+// var b = 200;
+// var c = 20;
+
+// var res = a + b + c;
+// if (res >= 90) {
+//   console.log("this is an obtuseangle");
+// } 
+// else
+// {
+//   console.log(" right angle ");
+// }
