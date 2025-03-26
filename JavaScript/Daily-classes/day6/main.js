8bae9510d897600de290ba2e4a5819d2ff402b32
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-26 14:19:53 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day6/main.js
@@ -0,0 +1,45 @@
+// indexOf , lastindexOf
+
+// let str = "webDeveloper we Develop websites Develop";
+// let res = str.lastIndexOf("Develop");
+// let res = str.indexOf("Develop");
+// let res = str.indexOf("Develop" , 25);
+// let res = str.charAt(2)
+// console.log(res);
+
+//strings concatenation
+// var string1 = "prep";
+// var string2 = " bytes" ;
+
+// concat method
+// var adding = string1.concat(string2)
+//  var adding = string1 + string2;
+//  console.log(adding);
+
+// SLICING METHOD
+
+// let newMsg = "learning slice method";
+// let result = newMsg.slice(8 , 15) //we can also use negetive value
+// console.log(result);
+
+// let subStr = "       hey we are trying  hey .. its a substring method..";
+// let message = subStr.substring(1, 15); //substring method
+// let res = subStr.replace("hey" , "hello") // replace method
+// let res = subStr.replaceAll("hey" , "hello")
+// let res = subStr.trim(); // trim method
+// console.log(res);
+
+
+
+
+// split method//
+
+// let subStr = " hey we are trying  hey .. its a substring method..";
+// let string = subStr.split(" ")
+// let string = (subStr.length)
+
+// let subStr = [12 , 67, 78];
+// let string = subStr.reverse()
+
+console.log(string);
+
