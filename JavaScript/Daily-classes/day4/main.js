fd9a83ef5f9f278af9254122cb689ac071910374
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-03-26 14:17:40 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day4/main.js
@@ -0,0 +1,206 @@
+// switch cases
+// we can define multiple cases
+//  let marks = prompt("enter your marks");
+//  let grade ;
+
+// switch (true) {
+//     case marks >= 41 && marks <= 50:
+//         grade = "A";
+
+//         break;
+
+//         case marks >= 31 && marks <= 40:
+//             grade = "B";
+
+//             break;
+
+
+//             case marks >= 21 && marks <= 30:
+//             grade = "C";
+
+//             break;
+
+//             case marks >= 11 && marks <= 20:
+//             grade = "D";
+
+//             break;
+
+//             case marks >= 0 && marks <= 10:
+//             grade = "E";
+
+//             break;
+
+
+
+
+//     default:
+//         grade = " invalid marks";
+//         break;
+// }
+
+// console.log(grade);
+
+
+
+// let day = prompt("enter the weekday");
+// let  weekday;
+
+// switch (true) {
+//     case day === "monday":
+//         weekday = "  the day is 1";
+
+//         break;
+
+
+//     case day === "tuesday":
+//         weekday = "  the day is 2";
+
+//         break;
+
+//     case day === "wednesday":
+//         weekday = " 3";
+
+//         break;
+//     case day === "thursday":
+//         weekday = "4";
+
+//         break;
+//     case day === "friday":
+//         weekday = "5";
+
+//         break;
+//     case day === "saturday":
+//         weekday = "6";
+
+//         break;
+//     case day === "sunday":
+//         weekday = "7";
+
+//         break;
+
+
+
+
+//     default:
+//         break;
+// }
+// console.log(weekday);
+
+
+
+
+// let day = prompt("enter the weekday");
+// let  weekday;
+
+// switch (true) {
+//     case day === "1":
+//         weekday = "  monday";
+
+//         break;
+
+
+//     case day === "2":
+//         weekday = "  tuesday";
+
+//         break;
+
+//     case day === "3":
+//         weekday = " wednesday";
+
+//         break;
+//     case day === "4":
+//         weekday = "thursday";
+
+//         break;
+//     case day === "5":
+//         weekday = "friday";
+
+//         break;
+//     case day === "6":
+//         weekday = "saturday";
+
+//         break;
+//     case day === "7":
+//         weekday = "sunday";
+
+//         break;
+//     default:
+//         break;
+// }
+// console.log(weekday);
+
+
+
+
+
+
+
+
+// for months//
+
+// let month = prompt("enter the month");
+// let monthName;
+
+// switch (true) {
+//     case month === "1":
+//         monthName = "  january";
+
+//         break;
+
+
+//     case month === "2":
+//         monthName = "  february";
+
+//         break;
+
+//     case month === "3":
+//         monthName = " march";
+
+//         break;
+//     case month === "4":
+//         monthName = "april";
+
+//         break;
+//     case month === "5":
+//         monthName = "may";
+
+//         break;
+//     case month === "6":
+//         monthName = "june";
+
+//         break;
+//     case month === "7":
+//         monthName = "july";
+
+//         break;
+//     case month === "8":
+//         monthName = "august";
+
+//         break;
+//     case month === "9":
+//         monthName = "september";
+
+//         break;
+//     case month === "10":
+//         monthName = "october";
+
+//         break;
+//     case month === "11":
+//         monthName = "november";
+
+//         break;
+//     case month === "12":
+//         monthName = "december";
+
+//         break;
+//     default:
+//         monthName = "invalid month name"
+//         break;
+// }
+// console.log(monthName);
+
+
+
+
+
+
