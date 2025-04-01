40fb23c4add049b155636bd9565812267217d518
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-04-01 13:18:42 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day9/main.js
@@ -0,0 +1,59 @@
+
+
+///finding vowels
+// let str = "indian" ;
+// let vowel = 0;
+// for (let i of str) {
+//     if(i == "a" || i == "e" || i ==="o" || i ==="i" || i ==="u" )
+//     {
+//         vowel++;
+//     }
+
+// }
+// console.log(vowel);
+
+
+
+// finding odd and even numbers
+
+// let arr = [4, 8, 7, 54, 55, 7, 2, 22, 1, 7];
+// let even = 0;
+// let odd = 0;
+// let oddnumber = [] ;
+// let evennum = [] ;
+
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     even++
+//     evennum.push(arr[i])
+    
+
+//   } else {
+//     odd++
+//     oddnumber.push(arr[i])
+//   }
+// }
+// console.log("count of even number is" + even);
+// console.log( "count of odd number is" + odd);
+
+
+// console.log( "even num " + evennum);
+// console.log("odd numbers" + oddnumber);
+
+
+// finding zero and ones
+// let arr1 = [0,1,1,0,0,,1,0,0];
+// let zero = 0;
+// let ones = 0;
+// for(i = 0 ; i < arr1.length ; i++){
+//     if(arr1[i] == 0){
+//         zero++
+//     }
+//     else {
+//         ones++
+//     }
+// }
+
+// console.log( "there are " + zero + " zero");
+// console.log("there are " + ones + " ones");
+
