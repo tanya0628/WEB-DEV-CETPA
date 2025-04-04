992704cc3c40ae1a27ade1cd3abc1368b5a83e08
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-04-04 13:50:32 +0530

    Add files via upload

---
+++ JavaScript/Assignments/Assignment3/main.js
@@ -0,0 +1,94 @@
+// JAVASCRIPT LOOP ASSGINMENT
+
+// Q1. Write a JavaScript program to construct the     following pattern, using a nested for loop.
+//       *
+//       * *
+//       * * *
+//       * * * *
+//       * * * * *
+
+// let row =   5 ;
+// for( let i = 1; i <= 5 ; i++){
+//     let pattern = " ";
+//     for (let j = 1; j <= i; j++) {
+//         pattern+=   '* '; // Add a '*' followed by a space
+//     }
+
+//     console.log(pattern);
+// }
+
+// Q2. Iterate through all numbers from 1 to 45. Print the following:
+
+//    For multiples of 3 print “Fizz”
+//    For multiples of 5 print “Buzz”
+//    For multiples of 3 and 5 print “FizzBuzz”
+
+// for (i = 1; i <= 45; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+// Q3. Calculate the sum of all the numbers in the following array
+
+// var numbersArray = [1,13,22,123,49];
+// var sum = 0;
+//  for( let  i =  0 ; i < numbersArray.length ; i++){
+//     sum+= numbersArray[i];
+//  }
+//  console.log(sum);
+
+
+
+
+
+// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; 
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; 
+
+// var sum = 0;
+//  for( let  i =  0 ; i < arr_1.length ; i++){
+//     sum+= arr_1[i];
+//  }
+//  console.log(sum);
+
+//  var sum1 = 0;
+//  for( let  i =  0 ; i < arr_2.length ; i++){
+//     sum1+= arr_2[i];
+//  }
+//  console.log(sum1);
+//  let sum3 = sum + sum1
+
+//  console.log( "sum of 2 arrays  "  + sum3);
+ 
+
+
+
+
+// Q5.Using a for loop output the elements in reverse order
+
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+//   Example output: 
+//   true 3.5  be  false cannot  true 9 what 43 OR each item   on a new line
+
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+// let res = arr.reverse() 
+// console.log(res);
+
+
+
+
+
+
+
+ 
+
+
+
+
