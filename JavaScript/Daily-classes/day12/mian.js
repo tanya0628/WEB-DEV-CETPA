a57d2741b5a1df959cf5db85968938dfdb7c2b9a
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-04-10 12:20:11 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day12/mian.js
@@ -0,0 +1,65 @@
+// function clock(){
+//     var date =  new Date() ;
+//     var h = date.getHours() ;
+//     var m = date.getMinutes() ;
+//     var s = date.getSeconds();
+
+
+//     let time = h + ":" + m +":" + s + " ";
+//        setTimeout(() => {   // using timeout to increase  seconds
+//         clock()             // calling function
+//     }, 1000);
+//     document.getElementById("MyClock").innerText = time;
+
+// }
+// clock(); 
+
+
+//print the counting 
+//   let count = 1 ;
+//   function nex(){
+//     if(count > 10)return;
+//         console.log(count) ;
+//         setTimeout( nex, count*1000)
+//         count++;
+        
+//     }
+  
+// //   nex()
+// for( let i = 1 ; i <= 10 ; i++){
+//     setTimeout(() => {
+
+    
+//     console.log(i);
+// } , i*1000)  
+// }    
+    
+
+
+// let count = 1;
+
+// function printCount() {
+//   if (count > 10) return;
+  
+//   console.log(count);
+//   count++;
+
+//   setTimeout(printCount, 1000);
+// }
+
+// printCount();
+
+
+
+// let count = 1;
+// function hello() {
+//     if (count > 15) return;
+//     console.log(count);
+//     setTimeout(hello , 2000)
+//     count++;
+    
+// }
+// hello()
+
+
+
