af5e9a42b16b70b8317e1a8b65969ba8e66bbad3
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-04-10 12:19:30 +0530

    Add files via upload

---
+++ JavaScript/Daily-classes/day11/main.js
@@ -0,0 +1,27 @@
+// function change(){
+//     let element = document.getElementsByTagName("h1");
+//     element[1].innerText = "hello world";
+//     element[1].style.backgroundColor = "red";
+// }
+
+// let ele1 = document.getElementById("param");
+// ele1.innerHTML = "hey i am param;"
+
+// let button = document.getElementsByClassName("btn");
+// button.addEventListener("onclick" (change()) )
+    
+// function change() {
+//     let cards = document.getElementsByClassName("main")
+//     cards[0].style.flexDirection = "column";
+//     let ele2 = document.getElementsByClassName("card");
+//     ele2[0].style.backgroundColor = "yellow";
+//     ele2[1].style.backgroundColor = "yellow";
+//     ele2[2].style.backgroundColor = "yellow";
+
+// }
+
+
+function text(){
+    let ele = document.getElementsByTagName("option");
+    ele[0].innerText = "apple"
+}
