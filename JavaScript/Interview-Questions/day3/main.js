f5b318e97951b3ef545a3b70e85ed5c76daf6bb4
Tanya Chauhan <tanyachauhan0628@gmail.com>
2025-04-12 16:39:26 +0530

    Add files via upload

---
+++ JavaScript/Interview-Questions/day3/main.js
@@ -0,0 +1,80 @@
+// Create a button , on click of which new Heading tag h1 should be added with text as "MERN stack" on the screen above button
+//  function change(){
+//     let heading = document.getElementsByTagName("h1")
+//     heading[0].innerText = "MERN STACK";
+//  }
+
+// Write code to get 1st H1 element from a webpage using DOM
+
+//  Get the first <h1> element on the page
+// const firstH1 = document.querySelector('h1');
+
+// // Check if it exists and log it
+// if (firstH1) {
+//   console.log(firstH1.textContent);
+// } else {
+//   console.log('No <h1> element found.');
+// }
+
+// Write code to implement timer clock using JS -- display HH:MM:SS -- the time should keep updating every second
+
+
+
+
+
+// function clock() {
+//   var date = new Date();
+//   var h = date.getHours();
+//   var m = date.getMinutes();
+//   var s = date.getSeconds();
+
+//   // let time = ${h} : ${m} : ${s};
+//   let time = h + " : " + m + " : " + s;
+//   setTimeout(() => {
+//     clock();
+//   }, 1000);
+//   document.getElementById("Myclock").innerText = time;
+// }
+// clock();
+
+
+
+
+
+
+
+// function newClock(){
+//     var hello = new Date();
+//     var h = hello.getHours();
+//     var m = hello.getMinutes();
+//     var s = hello.getSeconds();
+
+
+//     let time = h + ":" + m + ":" + s ;
+//     setTimeout(() => {
+//         newClock();
+//     }, 1000);
+//     document.getElementById("Myclock").innerText = time;
+// }
+// newClock();
+
+
+// Create an HTML page having content as Hello world and a button named Replace Text. When user will click on this button page content should be changed to "Welcome to Elevation academy"
+
+// function change(){
+//     let ele = document.getElementsByTagName("h1");
+//     ele[0].innerText = "Welcome to Elevation academy";
+// }
+
+
+// Create an HTML page having content as Hello world and a button named "Hide Text." When user will click on this button hide the "Hello World" text
+
+// function change(){
+//     let ele = document.getElementsByTagName("h1");
+//     ele[0].style.display = "none";
+// }
+
+
+
+
+
